fd41e42cad70f877f319c129ba93c98a8eb38902
Heimir Þór <Bangsadrengur@users.noreply.github.com>
2016-05-25 22:46:38 +0000

    Create The Descent.cpp

---
+++ EASY/The Descent.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+
+    // game loop
+    while (1) {
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < 8; i++) {
+            int mountainH; // represents the height of one mountain, from 9 to 0.
+            cin >> mountainH; cin.ignore();
+            
+            if(mountainH > max) {
+                max = mountainH;
+                index = i;
+            }
+        }
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        cout << index << endl; // The number of the mountain to fire on.
+    }
+}
